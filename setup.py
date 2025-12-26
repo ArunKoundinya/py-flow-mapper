7c0c8f0fadba7fea664c0b44277ef61592386626
Arun Koundinya <parasa.arunkoundinya@gmail.com>
2025-12-26 15:04:10 -0500

    updating setup.py to include new dependencies

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="py-flow-mapper",
-    version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    version="0.1.0-beta",
+    author="Arun Koundinya Parasa",
+    author_email="parasa.arunkoundinya@gmail.com",
     description="Python project analyzer and visualization tool",
     long_description=long_description,
     long_description_content_type="text/markdown",
